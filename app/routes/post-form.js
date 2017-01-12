389c58edd4006bc39dd89f990eec2a765c546e8e
ea <elliea915@gmail.com>
2017-01-11 19:42:29 -0800

    user can make new posts

--- app/routes/post-form.js
+++ app/routes/post-form.js
@@ -1,10 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  // actions: {
-  // savePost(params) {
-  //   var newPost = this.store.createRecord('post', params);
-  //   newPost.save();
-  //   this.transitionTo('index');
-  // }
+  actions : {
+    savePost(params) {
+      console.log(params);
+      var newPost = this.store.createRecord('post', params);
+      newPost.save();
+      this.transitionTo('index');
+    }
+  }
 });
